bea685a14717b05c5f99f70205cd622a714b6e03
George <Gwhyte@qw.com>
2021-08-04 00:03:02 +0100

    first back end upload

---
+++ src/main/java/com/qa/springproj/domain/ToDo.java
@@ -0,0 +1,86 @@
+package com.qa.springproj.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "todo")
+public class ToDo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	private String name;
+
+	@Column(name = "priority")
+	private int priority;
+
+	public ToDo() {
+
+	}
+
+	public ToDo(int id, String name, int priority) {
+		this.id = id;
+		this.name = name;
+		this.priority = priority;
+
+	}
+	
+	public ToDo(String name, int priority) {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, priority);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ToDo other = (ToDo) obj;
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(priority, other.priority);
+	}
+
+	
+
+}
